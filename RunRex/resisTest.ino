4f389cf230e7d3fb2b930a5b909312e0a410f09a
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2015-08-23 08:27:27 -0500

    trying to wire up keypad, added simple resistance tester

---
+++ RunRex/resisTest.ino
@@ -0,0 +1,19 @@
+int analogPin = 0;
+int raw = 0;
+float vOut = 0;
+
+void setup()
+{
+ Serial.begin(9600);
+ digitalWrite(13, HIGH);
+}
+
+void loop()
+{
+ raw = analogRead(analogPin);
+ Serial.print("Voltage: ");
+ Serial.println(vOut);
+ Serial.print("Resistance: ");
+ Serial.println(raw);
+ delay(1000);
+}
